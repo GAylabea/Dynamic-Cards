250f9355d9d2844d35cad9b5290148f5d821c0f9
GAylabea <gbea@rocketmail.com>
2016-03-05 12:43:09 -0600

    initial files added and variables added to js

---
+++ cards.js
@@ -0,0 +1,10 @@
+// Create variables for all of our html elements
+var createButton = document.getElementById("create");
+var deleteButton = document.getElementById("delete"); 
+var input = document.getElementById("input");
+var container = document.getElementById("container");
+// This variable needs to be ClassName, not ID and will return an array (since it's several cards)
+var card = document.getElementsClassName("card"); 
+
+
+
